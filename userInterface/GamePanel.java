af637155afef41ae5f9f21a857df4e412abdccc2
Goldenglow1427 <magician1427@gmail.com>
2024-05-20 11:45:45 -0400

    Adding photo of the cactus.

--- userInterface/GamePanel.java
+++ userInterface/GamePanel.java
@@ -77,9 +77,15 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener
         // Draw cacti
         ArrayList<Rectangle> cacti = be.getCactus();
         g.setColor(Color.GREEN);
-        for (Rectangle cactus : cacti) {
-            g.fillRect(cactus.x, cactus.y, cactus.width, cactus.height);
-        }
+        for(Rectangle cactus: cacti)
+            g.drawImage(plantImage, cactus.x, cactus.y, this);
+            // g.fillRect(cactus.x, cactus.y, cactus.width, cactus.height);
+
+        // Draw points
+        g.setColor(Color.BLUE);
+
+        g.setFont(new Font("TimesRoman", Font.PLAIN, 14));
+        g.drawString(String.format("Score: %d", be.getPoints()), 10, 20);
     }
 
     @Override
